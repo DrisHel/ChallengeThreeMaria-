9a56b0cab8e16666b5b2c0f1c151afe119b101b0
Doris Nunes <doris90nunes@gmail.com>
2022-01-26 17:09:40 -0300

    feat (constructor)created the builder to export the file.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = require('./routes/index');
+require('./infra/database/myBd');
 
 class App {
-    constructor(){
-        this.server = express();
-        
-    }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    router(this.server);
+  }
 }
 
 module.exports = new App().server;
