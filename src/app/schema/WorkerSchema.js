1a02cfde9aef90985502bc55feee3f63ec2ee789
Doris Nunes <doris90nunes@gmail.com>
2022-01-26 17:15:29 -0300

    refactor (removed field)removed id field from schema, as it is generating automatic.

--- src/app/schema/WorkerSchema.js
+++ src/app/schema/WorkerSchema.js
@@ -1,51 +1,40 @@
-const { mongoose } = require("mongoose");
-const mongoosePaginate = require('mongoose-paginate-v2');
-const { randonUUID }= require('crypto');
-
-
+const mongoose = require('mongoose');
+// const mongoosePaginate = require('mongoose-paginate-v2');
 
 const Worker = mongoose.Schema(
-{
-
-    id:{
-        type: String,
-        required:true,
-        default:randonUUID
+  {
+    name: {
+      type: String,
+      required: true
     },
 
-    name:{
-        type: String,
-        required:true
+    cpf: {
+      type: String,
+      required: true,
+      unique: true
     },
 
-    cpf:{
-        type: String,
-        required:true,
-        unique:true
+    birthday: {
+      type: Date,
+      required: true
     },
 
-    birthday:{
-        type: Date,
-        required:true
-    },
-
-    office:{
-        type: String,
-        enum: ['manager','seller','cashier'],  //gerente, vendedor, caixa
-        required:true
+    office: {
+      type: String,
+      enum: ['manager', 'seller', 'cashier'], // gerente, vendedor, caixa
+      required: true
     },
 
     situation: {
-        type: String,
-        required: true,
-        default: 'active'
-      }
-},
-      {
-          timestemps:true
-      } 
-
+      type: String,
+      required: true,
+      default: 'active'
+    }
+  },
+  {
+    timestamps: true
+  }
 );
 
-Worker.plugin(mongoosePaginate);
-module.exports= mongoose.model('Worker', Worker);
+// Worker.plugin(mongoosePaginate);
+module.exports = mongoose.model(' worker', Worker);
