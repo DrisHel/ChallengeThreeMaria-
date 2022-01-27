a2be65389615020aff9645b069d8f790d5cc57cc
Doris Nunes <doris90nunes@gmail.com>
2022-01-27 12:12:54 -0300

    feat ( Crud worker)search, change and delete route creation.

--- src/routes/workerRoutes.js
+++ src/routes/workerRoutes.js
@@ -2,5 +2,8 @@ const WorkerController = require('../app/controller/workerController');
 
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
   routes.post('/', WorkerController.create);
+  routes.get('/', WorkerController.getAll);
+  routes.put('/:id', WorkerController.update);
+  routes.delete('/:id', WorkerController.delete);
   server.use(prefix, routes);
 };
