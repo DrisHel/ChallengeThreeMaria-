60163bfa4fc709d5a3eb43d1071718fb1498e8c7
Doris Nunes <doris90nunes@gmail.com>
2022-01-27 12:11:10 -0300

    feat ( Crud worker)created search, change and delete, basic operations.

--- src/app/service/workerService.js
+++ src/app/service/workerService.js
@@ -5,6 +5,20 @@ class WorkerService {
     const result = await WorkerRepository.create(payload);
     return result;
   }
+
+  async listAll(search) {
+    return WorkerRepository.find(search);
+  }
+
+  async updateWorker(id, payload) {
+    const result = await WorkerRepository.updateById(id, payload);
+    return result;
+  }
+
+  async deleteWorker(id) {
+    const result = await WorkerRepository.deleteById(id);
+    return result;
+  }
 }
 
 module.exports = new WorkerService();
