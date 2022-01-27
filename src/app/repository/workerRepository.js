0473becd8f98001e9cae740a53616b4d6f9862b0
Doris Nunes <doris90nunes@gmail.com>
2022-01-27 12:10:07 -0300

    feat ( Crud )created search, change and delete, basic operations.

--- src/app/repository/workerRepository.js
+++ src/app/repository/workerRepository.js
@@ -4,5 +4,17 @@ class WorkerRepository {
   async create(payload) {
     return schemaWorker.create(payload);
   }
+
+  async find(search) {
+    return schemaWorker.find(search);
+  }
+
+  async updateById(id, payload) {
+    return schemaWorker.findByIdAndUpdate(id, { ...payload });
+  }
+
+  async deleteById(id) {
+    return schemaWorker.findByIdAndDelete(id);
+  }
 }
 module.exports = new WorkerRepository();
