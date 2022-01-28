581afad9e48274ffbd0ddac0f1c874e34d829a50
Doris Nunes <doris90nunes@gmail.com>
2022-01-28 16:54:42 -0300

    refactor ( schema )added missing items.

--- src/app/schema/WorkerSchema.js
+++ src/app/schema/WorkerSchema.js
@@ -1,40 +1,49 @@
 const mongoose = require('mongoose');
-// const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
-const Worker = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true
-    },
-
-    cpf: {
-      type: String,
-      required: true,
-      unique: true
-    },
-
-    birthday: {
-      type: Date,
-      required: true
-    },
-
-    office: {
-      type: String,
-      enum: ['manager', 'seller', 'cashier'], // gerente, vendedor, caixa
-      required: true
-    },
-
-    situation: {
-      type: String,
-      required: true,
-      default: 'active'
-    }
+const Worker = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: true
-  }
-);
 
-// Worker.plugin(mongoosePaginate);
+  cpf: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  birthday: {
+    type: Date,
+    required: true
+  },
+
+  office: {
+    type: String,
+    enum: ['manager', 'seller', 'cashier'], // gerente, vendedor, caixa
+    required: true
+  },
+
+  situation: {
+    type: String,
+    required: true,
+    default: 'active'
+  },
+  createdAt: {
+    type: Date,
+
+    required: true,
+
+    default: Date.now()
+  },
+  updatedAt: {
+    type: Date,
+
+    required: true,
+
+    default: Date.now()
+  }
+});
+
+Worker.plugin(mongoosePaginate);
 module.exports = mongoose.model('worker', Worker);
