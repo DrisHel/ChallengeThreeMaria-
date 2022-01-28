6f5539419a9f306094e7488032c5ffc9eda6644f
Doris Nunes <doris90nunes@gmail.com>
2022-01-28 16:47:20 -0300

    feat (file routes)route file editing.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const routerWorker = require('./workerRoutes');
+const routerproduct = require('./productRoutes');
 
 module.exports = (server) => {
   server.use((req, res, next) => {
     routerWorker(server, new Router());
+    routerproduct(server, new Router());
     next();
   });
 };
