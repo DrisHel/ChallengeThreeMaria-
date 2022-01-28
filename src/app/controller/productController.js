ec520377b1b988054977a632ce8a32d3b1534a3e
Doris Nunes <doris90nunes@gmail.com>
2022-01-28 16:44:48 -0300

    feat (product controller)product control file creation.

---
+++ src/app/controller/productController.js
@@ -0,0 +1,26 @@
+/* eslint-disable consistent-return */
+const { serializer, paginatedSerializer } = require('../serializer/productSerializer');
+const productService = require('../service/productService');
+
+class productController {
+  async create(req, res) {
+    try {
+      const result = await productService.create(req.body);
+      return res.status(201).json(serializer(result));
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  }
+
+  async getAll(req, res) {
+    try {
+      const result = await productService.listAll(req.query);
+      return res.status(200).json(paginatedSerializer(result));
+    } catch (error) {
+      res.status(400).json({ msg: error.message });
+    }
+  }
+}
+
+// eslint-disable-next-line new-cap
+module.exports = new productController();
