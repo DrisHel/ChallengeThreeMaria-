98ed506fbf159f85688ca558f6c12d99651c86a3
Doris Nunes <doris90nunes@gmail.com>
2022-01-28 10:01:36 -0300

    refactor ( removed _) removed _req _res.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const routerWorker = require('./workerRoutes');
 
 module.exports = (server) => {
-  server.use((_req, _res, next) => {
+  server.use((req, res, next) => {
     routerWorker(server, new Router());
     next();
   });
