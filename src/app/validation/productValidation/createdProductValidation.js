b1b62b43bc5b54624d368d28fb97383c2751849a
Doris Nunes <doris90nunes@gmail.com>
2022-01-31 20:14:18 -0300

    feat (fix)placed file inside folder.

---
+++ src/app/validation/productValidation/createdProductValidation.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+const BadRequest = require('../../error/BadRequest');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      name: Joi.string().min(4).max(50).required(),
+      category: Joi.string().required(),
+      price: Joi.string()
+        .regex(/^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$/)
+        .required(),
+      employee_id: Joi.string().required()
+    });
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) throw new BadRequest(error.message);
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      details: {
+        message: error.description
+      }
+    });
+  }
+};
