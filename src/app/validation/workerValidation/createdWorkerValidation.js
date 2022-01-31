307bb66db7d70d531f933931786768e9774616e2
Doris Nunes <doris90nunes@gmail.com>
2022-01-31 20:14:47 -0300

    feat (fix)placed file inside folder.

---
+++ src/app/validation/workerValidation/createdWorkerValidation.js
@@ -0,0 +1,23 @@
+const JoiDateExtension = require('@joi/date');
+const Joi = require('joi').extend(JoiDateExtension);
+const { enumWorker1 } = require('../../utils/enumWorker');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      name: Joi.string().min(10).max(40).required(),
+      cpf: Joi.string().length(11).required(),
+      birthday: Joi.date().format('DD/MM/YYYY').less(Date.now()).required(),
+      office: Joi.string()
+        .valid(...enumWorker1)
+        .trim()
+        .required()
+    });
+
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
