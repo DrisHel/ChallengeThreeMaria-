e4c6d47064b66fe910f14339c72bdd300e1ab5b0
Doris Nunes <doris90nunes@gmail.com>
2022-01-31 10:00:33 -0300

    feat ( added)adicionado enumWorker no arquivo.

--- src/app/validation/workerValidation.js
+++ src/app/validation/workerValidation.js
@@ -1,5 +1,6 @@
 const JoiDateExtension = require('@joi/date');
 const Joi = require('joi').extend(JoiDateExtension);
+const { enumWorker } = require('../utils/enumWorker');
 
 module.exports = async (req, res, next) => {
   try {
@@ -7,7 +8,10 @@ module.exports = async (req, res, next) => {
       name: Joi.string().min(10).max(40).required(),
       cpf: Joi.string().length(11).required(),
       birthday: Joi.date().format('DD/MM/YYYY').less(Date.now()).required(),
-      office: Joi.string().required().valid('manager', 'seller', 'cashier')
+      office: Joi.string()
+        .valid(...enumWorker)
+        .trim()
+        .required()
     });
 
     const { error } = await schema.validate(req.body, { abortEarly: false });
