81548398a0984d05c98e054e52cf6281bc44043b
Doris Nunes <doris90nunes@gmail.com>
2022-01-31 20:26:24 -0300

    feat (added routes)added get route to listOne.

--- src/routes/workerRoutes.js
+++ src/routes/workerRoutes.js
@@ -1,8 +1,10 @@
 const WorkerController = require('../app/controller/workerController');
+const createdWorkerValidation = require('../app/validation/workerValidation/createdWorkerValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
-  routes.post('/', WorkerController.create);
+  routes.post('/', createdWorkerValidation, WorkerController.create);
   routes.get('/', WorkerController.getAll);
+  routes.get('/:id', WorkerController.listOne);
   routes.put('/:id', WorkerController.update);
   routes.delete('/:id', WorkerController.delete);
   server.use(prefix, routes);
