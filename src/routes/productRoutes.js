203f3b6fcbcaa5b3674ed919012881f34d3e6545
Doris Nunes <doris90nunes@gmail.com>
2022-01-31 20:22:18 -0300

    feat ( added id)added id in post route.

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,7 +1,8 @@
 const productController = require('../app/controller/productController');
+const createdProductValidation = require('../app/validation/productValidation/createdProductValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/product') => {
-  routes.post('/', productController.create);
+  routes.post('/:id', createdProductValidation, productController.create);
   routes.get('/', productController.getAll);
   server.use(prefix, routes);
 };
