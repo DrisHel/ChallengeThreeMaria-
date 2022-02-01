ee2dee00681f7f228247c6f335dd1e02b6a1a71f
Doris Nunes <doris90nunes@gmail.com>
2022-02-01 08:39:01 -0300

    feat ( validation product )created validation of findAll not working.

---
+++ src/app/validation/productValidation/findAllProductValidation.js
@@ -0,0 +1,22 @@
+const Joi = require('joi').extend(require('@joi/date'));
+const BadRequest = require('../../error/BadRequest');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      name: Joi.string().min(4).max(50),
+      min_price: Joi.number().optional(),
+      max_price: Joi.number().optional()
+    });
+    const { error } = schema.validate(req.query, { abortEarly: false });
+    if (error) throw new BadRequest(error.message);
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      details: {
+        message: error.description
+      }
+    });
+  }
+};
