cd5902d185aed93d92e3f29e1782deb18da2ac36
Doris Nunes <doris90nunes@gmail.com>
2022-02-01 10:01:35 -0300

    feat (route get validation)added to the validation route the findAllvalidation.

--- src/routes/workerRoutes.js
+++ src/routes/workerRoutes.js
@@ -1,9 +1,10 @@
 const WorkerController = require('../app/controller/workerController');
 const createdWorkerValidation = require('../app/validation/workerValidation/createdWorkerValidation');
+const findAllvalidation = require('../app/validation/workerValidation/findAllvalidation');
 
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
   routes.post('/', createdWorkerValidation, WorkerController.create);
-  routes.get('/', WorkerController.getAll);
+  routes.get('/', findAllvalidation, WorkerController.getAll);
   routes.get('/:id', WorkerController.listOne);
   routes.put('/:id', WorkerController.update);
   routes.delete('/:id', WorkerController.delete);
