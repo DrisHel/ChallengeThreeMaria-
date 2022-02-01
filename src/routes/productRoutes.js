a817359783405a3c480b35ecf34751972fb5baed
Doris Nunes <doris90nunes@gmail.com>
2022-02-01 08:44:37 -0300

    refactor (comment)created comment for test route.

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,9 @@
 const productController = require('../app/controller/productController');
 const createdProductValidation = require('../app/validation/productValidation/createdProductValidation');
+const findAllProductValidation = require('../app/validation/productValidation/findAllProductValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/product') => {
-  routes.post('/:id', createdProductValidation, productController.create);
-  routes.get('/', productController.getAll);
+  routes.post('/', createdProductValidation, productController.create);
+  routes.get('/', findAllProductValidation, productController.getAll);
   server.use(prefix, routes);
 };
